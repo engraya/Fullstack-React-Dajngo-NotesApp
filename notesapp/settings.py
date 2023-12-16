5c41987756be16e583b7601fef64e669b3466398
Engraya <ahmadyakubuaya@gmail.com>
2023-12-16 16:03:27 +0100

    CORS Config

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -31,13 +31,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-     'api.apps.ApiConfig',
+    'api.apps.ApiConfig',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
